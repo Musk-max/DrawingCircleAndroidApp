f56f8c2cc0357d03612049f6fac23d8a70f672fb
Musk-max <76408168+Musk-max@users.noreply.github.com>
2024-10-22 16:47:18 +0200

    labelisation ou etiquettage des cercles

--- app/src/main/java/com/example/drawingcircle/DrawingCircle.kt
+++ app/src/main/java/com/example/drawingcircle/DrawingCircle.kt
@@ -2,6 +2,8 @@ package com.example.drawingcircle
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Color.BLACK
 import android.graphics.Color.RED
 import android.graphics.Paint
 import android.util.AttributeSet
@@ -10,16 +12,25 @@ import android.view.View
 
 class DrawingCircle(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
      val mainPaint:Paint
+     val textPaint:Paint
     init {
         mainPaint=Paint()
         mainPaint.color=RED
+        // On créé un deuxième text Paint pour pouvoir afficher le text dans une couleur différente
+        textPaint=Paint()
+        textPaint.color= Color.BLACK
+        textPaint.setTextSize(50f);
    }
 
     var xpos=0f
     var ypos=0f
 
     override fun onDraw(canvas: Canvas) {
+        //juste pour modifier la couleur de la vue
+
         canvas.drawCircle(xpos,ypos,100f,mainPaint)
+
+        canvas.drawText("Some Text", xpos, ypos, textPaint);
     }
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
